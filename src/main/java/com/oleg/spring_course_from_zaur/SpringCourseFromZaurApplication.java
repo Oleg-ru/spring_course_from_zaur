0da11061d0d637698a9666a915813b4b84e1d455
Oleg <oleg_09-95@mail.ru>
2021-12-10 22:40:09 +0300

    Lesson 12 Добавлено: Работа с Spring Container IoC, DI Работа устаревшим способом -> конфигурирования бинов через XML file

---
+++ src/main/java/com/oleg/spring_course_from_zaur/SpringCourseFromZaurApplication.java
@@ -0,0 +1,13 @@
+package com.oleg.spring_course_from_zaur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseFromZaurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseFromZaurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oleg/spring_course_from_zaur/SpringCourseFromZaurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleg.spring_course_from_zaur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseFromZaurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
