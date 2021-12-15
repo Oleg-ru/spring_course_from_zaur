8b6192a92472ec8f4a8e2a6f74cbee7193578cd2
Oleg <oleg_09-95@mail.ru>
2021-12-15 20:06:36 +0300

    Lesson 32. Работа с  @AfterThrowing и @After

--- src/main/java/aop/Test2.java
+++ src/main/java/aop/Test2.java
@@ -8,10 +8,13 @@ import java.util.List;
 public class Test2 {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-
         University university = context.getBean("university", University.class);
         university.addStudents();
-        List<Student> students = university.getStudents();
-        System.out.println(students);
+        try {
+            List<Student> students = university.getStudents();
+            System.out.println(students);
+        } catch (Exception e) {
+            System.out.println("Было выброшенно исключение: " + e);
+        }
     }
 }
--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,9 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,21 +10,30 @@ import java.util.List;
 @Aspect
 public class UniversityLoggingAspect {
 
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsAdvice() {
-        System.out.println("beforeGetStudentsAdvice: логируем получение списка студентов" +
-                " перед методом getStudents");
-    }
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsAdvice() {
+//        System.out.println("beforeGetStudentsAdvice: логируем получение списка студентов" +
+//                " перед методом getStudents");
+//    }
+//
+//    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+//    public void afterReturningGetStudentsAdvice(List<Student> students) {
+//
+//        Student student1 = students.get(0);
+//        student1.setNameSurname("Mr. " + student1.getNameSurname());
+//        student1.setAvgGrade(student1.getAvgGrade() + 1);
+//
+//        System.out.println("afterReturningGetStudentsAdvice: логируем получение списка студентов" +
+//                " после работы метода getStudents");
+//    }
 
-    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
-    public void afterReturningGetStudentsAdvice(List<Student> students) {
+//    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "throwable")
+//    public void afterThrowingGetStudentsAdvice(Throwable throwable) {
+//        System.out.println("afterThrowingGetStudentsAdvice: логируем выброс исключения : " + throwable);
+//    }
 
-        Student student1 = students.get(0);
-        student1.setNameSurname("Mr. " + student1.getNameSurname());
-        student1.setAvgGrade(student1.getAvgGrade() + 1);
-
-
-        System.out.println("afterReturningGetStudentsAdvice: логируем получение списка студентов" +
-                " после работы метода getStudents");
+    @After("execution(* getStudents())")
+    public void afterGetStudentsAdvice() {
+        System.out.println("afterGetStudentsAdvice: логирую нормальное\\Exception заверщение метода");
     }
 }
